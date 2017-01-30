1858a26bb68cdb578977603c82a224c4f3763c0a
Maksym <maxi007007@gmail.com>
2017-01-30 18:18:24 +0200

    Deploy

--- src/main/java/project/services/WorkWithCompany.java
+++ src/main/java/project/services/WorkWithCompany.java
@@ -11,7 +11,11 @@ import project.entities.Company;
 public class WorkWithCompany {
 
     public String getCompanyById(int id){
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring/root-context.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring/root-context.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring/appServlet/servlet-context.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("resources/applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
         CompanyDAO companyDAO = (CompanyDAO) context.getBean("companyDAO");
 
         Company company = companyDAO.selectCompanyById(id);
